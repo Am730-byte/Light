3629cc5241c2873ac40d1bdefd48b0d1be0deba0
aayush <aayush.meena2006@gmail.com>
2026-04-06 13:38:56 +0530

    add railway database setup scripts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations:{
-    seed: "ts-node prisma/seed.ts"
+    seed: "ts-node-esm prisma/seed.ts"
   },
   datasource: {
     url: env("DATABASE_URL"),
